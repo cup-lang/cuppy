55f228fb4bbba72d4a7806584e6e6d742143423a
jgrajewsky <jgrajewsky@airmail.cc>
2022-01-20 22:23:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ client.on('messageCreate', message => {
     } else if ((command.length === 3 && command.startsWith('run')) || command.startsWith('run ')) {
         if (command.length <= 4) {
             message.channel.send('error: missing argument [CODE]');
-            return;
         } else {
             let code = command.substr(4);
             if (code[0] == '`' && code[code.length - 1] == '`') {
@@ -87,8 +86,7 @@ client.on('messageCreate', message => {
             playgroundRunCode(message, code);
         }
     } else {
-        message.channel.send(`Command \`!${msg.split(' ')[0]}\` was not recognized. Type \`!help\` too see available commands.`);
-        return;
+        message.channel.send(`Command \`!${command.split(' ')[0]}\` was not recognized. Type \`!help\` too see available commands.`);
     }
 });
 
